14c35e45831e1dc625d2cc31a3fffea0134ad819
Renishlale <lalerenishcode@gmail.com>
2025-11-06 16:15:39 +0530

    Update 10.4.cpp

--- Exp 10/10.4.cpp
+++ Exp 10/10.4.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+ #include <iostream>
 using namespace std;
 
 template<class T>
@@ -6,21 +6,32 @@ class Stack {
     T st[10];
     int top;
 public:
-    Stack(){ top = -1; }
+    Stack() { top = -1; }
 
-    void push(T x){
-        st[++top] = x;
-        cout<<"Pushed: "<<x<<endl;
+    void push(T x) {
+        if (top == 9) {
+            cout << "Stack is full. Cannot push " << x << endl;
+        } else {
+            st[++top] = x;
+            cout << "Pushed: " << x << endl;
+        }
     }
-    void pop(){
-        cout<<"Popped: "<<st[top--]<<endl;
+
+    void pop() {
+        if (top == -1) {
+            cout << "Stack is empty. Cannot pop." << endl;
+        } else {
+            cout << "Popped: " << st[top--] << endl;
+        }
     }
 };
 
-int main(){
+int main() {
     Stack<int> s;
     s.push(10);
     s.push(20);
     s.pop();
     s.pop();
+    s.pop();
+    return 0;
 }
