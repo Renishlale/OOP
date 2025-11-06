521d7b10582a55bda727d9a12317047bc5edfa6b
Renishlale <lalerenishcode@gmail.com>
2025-11-06 16:23:11 +0530

    Update 10.4.cpp

--- Exp 10/10.4.cpp
+++ Exp 10/10.4.cpp
@@ -42,12 +42,9 @@ int main() {
     Stack<int> s;
     s.push(10);
     s.push(20);
-    s.push(30);
-    s.display();
     s.pop();
     s.display();
-    s.pop();
-    s.pop();
-    s.display();
+    
     return 0;
 }
+
