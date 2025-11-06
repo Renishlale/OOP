333c1b9bafe163671bbc63500960ad8299943d1f
Renishlale <lalerenishcode@gmail.com>
2025-11-06 16:22:07 +0530

    Update 10.4.cpp

--- Exp 10/10.4.cpp
+++ Exp 10/10.4.cpp
@@ -24,14 +24,30 @@ public:
             cout << "Popped: " << st[top--] << endl;
         }
     }
+
+    void display() {
+        if (top == -1) {
+            cout << "Stack is empty." << endl;
+        } else {
+            cout << "Stack elements: ";
+            for (int i = top; i >= 0; i--) {
+                cout << st[i] << " ";
+            }
+            cout << endl;
+        }
+    }
 };
 
 int main() {
     Stack<int> s;
     s.push(10);
     s.push(20);
+    s.push(30);
+    s.display();
+    s.pop();
+    s.display();
     s.pop();
     s.pop();
-    s.pop();
+    s.display();
     return 0;
 }
